2c301dff25bcc6f02b85428f3a563638e5f5230b
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2025-02-11 16:18:39 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,43 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
-
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
-
-// Classe principale, con metodo main
+import java.util.Random;
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
 
-        //Creo l'oggetto in per l'input da tastiera
+    public static int rimuoviElemento(int vettore[], int posizione) {
+        int i;
+        for (i=posizione; i<vettore.length; i++) {
+            vettore[i] = vettore[i+1];
+        }
+        return vettore.length-1;
+    }
+
+    public static void main(String args[]) {
         Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        Random random = new Random();
+        int N, coppie, cornici, i, j;
+        boolean trovato;
+        System.out.println("Inserisci il numero di listelli presenti nella cassettiera: ");
+        N = Integer.parseInt(in.nextLine());
+        int[] listelli = new int[N];
+        for (i=0; i<listelli.length; i++) {
+            listelli[i] = (random.nextInt(7))+4;
+        }
+        trovato = true;
+        coppie = 0;
+        while (N<=2 && trovato) {
+            trovato = false;
+            for (i=0; i<listelli.length-1; i++) {
+                for (j=i+1; j<listelli.length; j++) {
+                    if (listelli[i]==listelli[j]) {
+                        N = rimuoviElemento(listelli, i);
+                        N = rimuoviElemento(listelli, j);
+                        trovato = true;
+                        coppie++;
+                        i = 0;
+                    }
+                }
+            }
+        }
+        cornici = coppie/2;
+        System.out.println("L'artigiano ha fabbricato " + cornici + " cornici.");
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
